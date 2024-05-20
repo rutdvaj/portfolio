58038819f913b40fdab4026f4e10e43110c028bd
rutdvaj <54937822+rutdvaj@users.noreply.github.com>
2024-05-20 13:59:49 +0530

    issue fixed

--- src/app/landingPage/page.jsx
+++ src/app/landingPage/page.jsx
@@ -5,7 +5,7 @@ import { MenubarDemo } from "./Menubar";
 export default function Page() {
   return (
     <div>
-      <div className="menu flex justify-center items-center rounded-lg shadow-2xl shadow-black">
+      <div className="w-full menu flex justify-center items-center rounded-lg shadow-2xl shadow-black bg-mainColor">
         <MenubarDemo />
       </div>
       <div className="hs-1 flex items-center justify-center">
